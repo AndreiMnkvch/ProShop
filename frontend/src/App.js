0947d6aed1bf4a0779a2b499997d5cb0c9ddf95a
AndreiMnkvch <justaandrew@gmail.com>
2022-09-16 11:39:23 +0300

    implemented react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,25 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen';
+
+
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header/>
       <main className="py-3">
         <Container>
-          <HomeScreen/>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />}/>
+          </Routes>
         </Container>
       </main>
       <Footer/>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/products.js
+++ frontend/src/products.js
@@ -7,7 +7,7 @@ const products = [
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     'brand': 'Apple',
     'category': 'Electronics',
-    'price': 1999.99,
+    'price': 999.99,
     'countInStock': 10,
     'rating': 4.5,
     'numReviews': 12,
