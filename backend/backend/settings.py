eb0772b17fedff0075d5b6135f801fec249821f3
AndreiMnkvch <justaandrew@gmail.com>
2022-09-17 18:09:55 +0300

    set up staticfiles

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,7 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# static_url is a prefix that didn't appear in address bar. When user clicks on image
+# http://127.0.0.1:8000/images/phone.jpg but actual address is BASE_DIR/static/images/phone.jpg
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = 'static/images'
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- backend/base/models.py
+++ backend/base/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, blank=True)
-    # image
+    image = models.ImageField(blank=True)
     brand = models.CharField(max_length=200, blank=True)
     category = models.CharField(max_length=200, blank=True)
     description = models.TextField(blank=True)
@@ -57,7 +57,7 @@ class OrderItem(models.Model):
     name = models.CharField(max_length=200, blank=True)
     qty = models.IntegerField(blank=True, null=True, default=0)
     price = models.DecimalField(max_digits=7, blank=True, null=True, decimal_places=2)
-    image = models.CharField(max_length=200, blank=True)
+    image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.name
