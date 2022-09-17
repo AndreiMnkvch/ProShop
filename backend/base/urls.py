a64921ab4639bfd9fa5f64c517178fbeb102206f
AndreiMnkvch <justaandrew@gmail.com>
2022-09-17 11:16:53 +0300

    fetched data from backend using axios

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,5 +3,5 @@ from .views import get_route, get_products, get_product
 urlpatterns = [
     path('', get_route, name="routes"),
     path('products/', get_products, name="products"),
-    path('products/<int:pk>', get_product, name="product")
+    path('products/<str:pk>', get_product, name="product")
 ]
