1a80f0c60a4783a3063665e6037a757ab0cf155a
AndreiMnkvch <justaandrew@gmail.com>
2022-09-17 18:56:20 +0300

    added ProductSerializer, updated get_product & get_products views

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,10 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Product
 from .products import products
+from .serializers import ProductSerializer
 
 
 @api_view(['GET'])
@@ -25,12 +28,12 @@ def get_route(request):
 
 @api_view(['GET', 'POST'])
 def get_products(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET', 'POST'])
 def get_product(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
