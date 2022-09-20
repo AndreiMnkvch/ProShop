9123b34dc23822aac51be01edf966f1537af4b52
AndreiMnkvch <justaandrew@gmail.com>
2022-09-20 10:38:05 +0300

    added and deployed into homescreen message component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector  } from 'react-redux'
 import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
 import Loader from '../components/Loader'
+import Message from '../components/Message'
+
 import { fetchProducts } from '../features/products/productsSlice'
 
 
@@ -25,12 +27,12 @@ function HomeScreen() {
     <div>
       <h1>Latest products </h1>
         { productsAreLoading ? <Loader />
-          : error ? <h3>{error}</h3>
+          : error ? <Message variant='danger'> {error}</Message>
             :
             <Row>
               { productItems.map(product => (
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                  <Product product={product} />
+                  <Product key={product._id} product={product} />
                 </Col>
                 ))
               }
