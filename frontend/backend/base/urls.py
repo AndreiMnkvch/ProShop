464eeddcb6ac4ea13cdeacd762cb257acd4c0b90
Andrei <justaandrew@gmail.com>
2023-10-30 11:13:22 +0100

    deleted redundant files after careless clonning from remote repo

--- frontend/backend/base/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from base.views import ProductViewSet
-
-
-router = routers.DefaultRouter()
-router.register(r'products', ProductViewSet)
-urlpatterns = router.urls
