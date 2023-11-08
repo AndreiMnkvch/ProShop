e5ca025510e16c32befaea2606f7ea379dd35418
Andrei <justaandrew@gmail.com>
2023-11-08 16:32:13 +0100

    redesigned models for smoother orders requests

--- backend/base/models.py
+++ backend/base/models.py
@@ -31,42 +31,43 @@ class Review(models.Model):
         return str(self.rating)
 
 
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    payment_method = models.CharField(max_length=200, blank=True)
-    tax_price = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
-    shipping_price = models.DecimalField(max_digits=7, blank=True, null=True, decimal_places=2)
-    total_price = models.DecimalField(max_digits=7, blank=True, null=True, decimal_places=2)
-    is_paid = models.BooleanField(default=False)
-    paid_at = models.DateTimeField(null=True, blank=True)
-    is_delivered = models.BooleanField(default=False)
-    delivered_at = models.DateTimeField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+class ShippingAddress(models.Model):
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    postal_code = models.IntegerField()
+    country = models.CharField(max_length=200)
 
     def __str__(self):
-        return str(self.createdAt)
+        return self.address
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.CASCADE)
+    payment_method = models.CharField(max_length=200, blank=True)
+    tax_price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    shipping_price = models.DecimalField(max_digits=7, default=0, decimal_places=2)
+    total_price = models.DecimalField(max_digits=7, decimal_places=2)
+    is_paid = models.BooleanField(default=False)
+    is_delivered = models.BooleanField(default=False)
+    delivered_at = models.DateTimeField(null=True, blank=True)
+    paid_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):  
+        return str(self.created_at)
 
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, related_name='order_items', on_delete=models.CASCADE)
     name = models.CharField(max_length=200, blank=True)
-    qty = models.IntegerField(blank=True, null=True, default=0)
-    price = models.DecimalField(max_digits=7, blank=True, null=True, decimal_places=2)
+    qty = models.IntegerField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
     image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.name
 
 
-class ShippingAddress(models.Model):
-    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True)
-    address = models.CharField(max_length=200, blank=True)
-    city = models.CharField(max_length=200, blank=True)
-    postal_code = models.IntegerField(blank=True, null=True, default=0)
-    country = models.CharField(max_length=200, blank=True)
-    shipping_price = models.DecimalField(max_digits=7, blank=True, null=True, decimal_places=2)
-
-    def __str__(self):
-        return self.address
 
