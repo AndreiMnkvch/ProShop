b69bcf6b056912ec82349524305cbabb50dc7e6c
AndreiMnkvch <justaandrew@gmail.com>
2023-11-12 20:34:20 +0100

    prepared orders viewset for pay

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,4 +1,11 @@
+from datetime import datetime
+
+from django.shortcuts import get_object_or_404
+
+from rest_framework import status 
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response 
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from base.serializers import ProductSerializer
@@ -17,3 +24,22 @@ class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = (IsAuthenticated,)
+
+    def list(self, request):
+        queryset = Order.objects.filter(user=request.user)
+        serializer = OrderSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        queryset = Order.objects.filter(user=request.user)
+        order = get_object_or_404(queryset, pk=pk)
+        serializer = OrderSerializer(order)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    @action(detail=True, methods=['patch'], url_path="pay")
+    def update_order_to_paid(self, request, pk=None):
+        order = self.get_object()
+        order.is_paid = True
+        order.paid_at = datetime.now()
+        order.save()
+        return Response("Succesfully updated to paid", status=status.HTTP_200_OK)
